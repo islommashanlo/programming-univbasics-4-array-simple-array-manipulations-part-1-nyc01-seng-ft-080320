acddafa469c7718046b3877f19dff5b771c6abc3
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-24 19:18:28 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+#push method
+def using_push(array, string)
+  array.push(string)
+end
+#unshift method
+def using_unshift(array, string)
+  array.unshift(string)
+end
+#pop method
+def using_pop(array)
+  array.pop
+end
+#pop_with_args
+def pop_with_args(array)
+  array.pop(2)
+end
+#shift method
+def using_shift(array)
+  array.shift
+end
+#shift_with_args
+def shift_with_args(array)
+  array.shift(2)
+end
